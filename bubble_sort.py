e743cb8d507b103dcd4cdf7d05ac6103845d103e
M <lmiguelvargasf@gmail.com>
2015-02-11 17:46:58 -0500

    Improved bubble_sort.py according to pylint.

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,30 +3,22 @@ Created on Oct 6, 2014
 
 @author: M
 '''
-    
-def bubble_sort(L):
+
+def bubble_sort(lst):
     """
     Sorts a list in increasing order. Because of lists are mutable
     this function does not have to return something.
     This algorithm uses bubble sort.
-    @param L: a list (in general unsorted)
+    @param lst: a list (in general unsorted)
     """
-    for i in range(len(L) - 1):
+    for i in range(len(lst) - 1):
         already_sorted = True
-        for j in range(len(L) - i - 1):
-            if L[j] > L[j + 1]:
-                already_sorted = False                                
-                L[j],  L[j + 1]= L[j + 1], L[j]
+        for j in range(len(lst) - i - 1):
+            if lst[j] > lst[j + 1]:
+                already_sorted = False
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
         # if no swaps were developed in the previous loop,
         # it means that the list is already sorted. Thus,
         # loop is exited and function terminates (returns None)
         if already_sorted:
             break
-
-
-
-list1 = [1, -2, 3, 2, 0, 4, -1, 2, 0, 1, -5, 5, -6]
-
-#list1 = [2, 1, -1]
-bubble_sort(list1)
-print(list1)
